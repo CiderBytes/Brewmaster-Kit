d836def5c99dc41daa808d1c5f33354a2ed54032
lutzy55 <3700070+lutzy55@users.noreply.github.com>
2021-05-13 14:11:38 -0400

    Add path for homebrew in Big Sur
    "export PATH=/opt/homebrew/bin:$PATH"

--- Ciderbytes Mac Setup.sh
+++ Ciderbytes Mac Setup.sh
@@ -94,6 +94,7 @@ if test ! $(which brew); then
     echo "Installing homebrew..."
     #ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+    export PATH=/opt/homebrew/bin:$PATH
 fi
 
 # Update homebrew recipes
